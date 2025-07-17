dddde678226987ae935c8ba1303a99b1b561f873
AbuBakar <muhammadabubakar19187@gmail.com>
2025-07-17 11:33:29 +0500

    Backend Started

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+
+const app = express();
+const PORT = process.env.PORT || 3000
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res)=>{
+    res.send('API WORKING')
+})
+
+
+
+app.listen(PORT, ()=>{
+    console.log(`Server is listening at PORT: ${PORT}`);
+})
