0197a8cbaaee1e80cb41fc49bfb28e78dacd20d9
AbuBakar <muhammadabubakar19187@gmail.com>
2025-07-18 22:53:11 +0500

    Start working on backend, controllers, models, routes

---
+++ backend/controllers/adminController.js
@@ -0,0 +1,15 @@
+
+
+
+// API FOR ADDING DOCTORS
+export const addDoctor = async () => {
+    try {
+        const {name, email, password, speciality, degree, experience, about, fees, address} = req.body;
+        const imageFile = req.file;
+
+        console.log({name, email, password, speciality, degree, experience, about, fees, address}, imageFile);
+        
+    } catch (error) {
+        
+    }
+}
--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,19 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/mongodb.js';
+import connectCloudinary from './config/cloudinary.js';
+import adminRouter from './routes/adminRoute.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000
 connectDB()
+connectCloudinary()
 
 app.use(express.json());
 app.use(cors());
 
+
+app.use('/api/admin', adminRouter)
 app.get('/', (req, res)=>{
     res.send('API WORKING')
 })
